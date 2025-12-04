83f1d9fc6425f58577f1b6b6158db05bf8212be0
Tina <tguessbacher@gmx.de>
2025-12-04 11:50:13 +0100

    Simple bouncing ball completed

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,63 @@
 module Main(main) where
 
 import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
 
 data AnimationState = AnimationState {
-    ballPosition :: (Float, Float),
-    ballVelocity :: (Float, Float)
+    position :: (Float, Float),
+    velocity :: (Float, Float)
 }
 
+windowWidth :: Int
+windowWidth = 320
+
+windowHeight :: Int
+windowHeight = 240
+
+windowPosX :: Int
+windowPosX = 10
+
+windowPosY :: Int
+windowPosY = 10
+
+circleRadius :: Int
+circleRadius = 10
+
 window :: Display
-window = InWindow "Bouncy balls" (320, 240) (10, 10)
+window = InWindow "Bouncy balls" (windowWidth, windowHeight) (windowPosX, windowPosY)
 
 background :: Color
 background = black
 
-frame :: Float -> Picture
-frame seconds = translate nx ny $ color white $ circleSolid 10
+renderer :: AnimationState -> Picture
+renderer as = translate x y $ color white $ circleSolid $ fromIntegral circleRadius
     where
-        x = 0
-        y = 0
-        vx = 10
-        vy = 10
-        nx = x + vx * seconds
-        ny = y + vy * seconds
+        x = fst $ position as
+        y = snd $ position as
 
+interator :: ViewPort -> Float -> AnimationState -> AnimationState
+interator _ deltaTime state = AnimationState {
+    position = (nposx, nposy),
+    velocity = (nvx, nvy)
+}
+    where
+        posx = fst $ position state
+        posy = snd $ position state
+        vx = fst $ velocity state
+        vy = snd $ velocity state
+        nvx = if abs posx >= fromIntegral windowWidth / 2 - fromIntegral circleRadius then -vx else vx
+        nvy = if abs posy >= fromIntegral windowHeight / 2 - fromIntegral circleRadius then -vy else vy
+        nposx = posx + nvx * deltaTime
+        nposy = posy + nvy * deltaTime
+
+fps :: Int
+fps = 60
+
+initialAnimationState :: AnimationState
+initialAnimationState = AnimationState {
+    position = (0, 0),
+    velocity = (10, 10)
+}
 
 main :: IO ()
-main = animate window background frame
+main = simulate window background fps initialAnimationState renderer interator
