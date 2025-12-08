0ef3e7ce506b155145fca3144d88b63a70450588
llamato <tguessbacher@gmx.de>
2025-12-08 04:02:09 +0100

    Added bug where first additional ball appears twice (handleWallCollision / iterator)

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@ module Main(main) where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
+import qualified Data.Map as M
+import Data.List (foldl')
+
 data Ball = Ball {
     position :: (Float, Float),
     velocity :: (Float, Float),
@@ -92,29 +95,106 @@ inputHandler _ as = GameState {
     currentMouseEvent = 0
 }
 
-iterator :: Float -> GameState -> GameState
-iterator deltaTime as = GameState {
-    balls = map (\ball -> let
-        posx = fst $ position ball
-        posy = snd $ position ball
-        vx = fst $ velocity ball
-        vy = snd $ velocity ball
-        nvx = if abs posx >= hborder then -vx else vx
-        nvy = if abs posy >= vborder then -vy else vy
-        nposx = posx + nvx * deltaTime
-        nposy = posy + nvy * deltaTime
-        ncolor = (currentColor ball+fromEnum (nvx /= vx || nvy /= vy)) `mod` length (colors as)
-        in Ball {
-        position = (nposx, nposy),
+areColliding :: Ball -> Ball -> Bool
+areColliding ball1 ball2 = let
+    (x1, y1) = position ball1
+    (x2, y2) = position ball2
+    dx = x1 - x2
+    dy = y1 - y2
+    distance = sqrt (dx*dx + dy*dy)
+    in distance < 2 * fromIntegral scaledCircleRadius
+
+handleCollision :: Ball -> Ball -> (Ball, Ball)
+handleCollision ball1 ball2 = let
+    (x1, y1) = position ball1
+    (x2, y2) = position ball2
+    (vx1, vy1) = velocity ball1
+    (vx2, vy2) = velocity ball2
+    (deltaX, deltaY) = (x2 - x1, y2 - y1)
+    distance = sqrt (deltaX * deltaX + deltaY * deltaY)
+    (normalX, normalY) = if distance > 0 then (deltaX/distance, deltaY/distance) else (1, 0)
+    (dvx, dvy) = (vx1 - vx2, vy1 - vy2)
+    velocityAlongNormal = dvx * normalX + dvy * normalY
+    in if velocityAlongNormal > 0 then (ball1, ball2) else let
+        impulse = 2 * fromIntegral scaledCircleRadius - distance
+        (newVx1, newVy1) = (vx1 - impulse * normalX, vy1 - impulse * normalY)
+        (newVx2, newVy2) = (vx2 + impulse * normalX, vy2 + impulse * normalY)
+
+        overlap = 2 * fromIntegral scaledCircleRadius - distance
+        separation = if distance > 0 then overlap / 2 else fromIntegral scaledCircleRadius
+        (sepX, sepY) = (separation * normalX, separation * normalY)
+
+        (newX1, newY1) = (x1 - sepX, y1 - sepY)
+        (newX2, newY2) = (x2 + sepX, y2 + sepY)
+    in
+        ( Ball {
+            position = (newX1, newY1),
+            velocity = (newVx1, newVy1),
+            currentColor = currentColor ball1
+            },
+            Ball {
+                position = (newX2, newY2),
+                velocity = (newVx2, newVy2),
+                currentColor = currentColor ball2
+            }
+        )
+
+processCollisions :: [Ball] -> [Ball]
+processCollisions balls = M.elems $ foldl' process (M.fromList (zip [0..] balls)) [0..length balls - 1]
+  where
+    process :: M.Map Int Ball -> Int -> M.Map Int Ball
+    process ballMap idx = 
+        case M.lookup idx ballMap of
+            Nothing -> ballMap
+            Just ball -> foldl' (checkIdx idx) ballMap [(idx+1)..M.size ballMap - 1]
+    
+    checkIdx :: Int -> M.Map Int Ball -> Int -> M.Map Int Ball
+    checkIdx idx ballMap otherIdx =
+        case (M.lookup idx ballMap, M.lookup otherIdx ballMap) of
+            (Just ball, Just otherBall) ->
+                if areColliding ball otherBall
+                then let (newBall, newOther) = handleCollision ball otherBall
+                     in M.insert idx newBall (M.insert otherIdx newOther ballMap)
+                else ballMap
+            _ -> ballMap
+
+handleWallCollision :: Ball -> Ball
+handleWallCollision ball = let
+    posx = fst $ position ball
+    posy = snd $ position ball
+    vx = fst $ velocity ball
+    vy = snd $ velocity ball
+    nvx = if abs posx >= hborder then -vx else vx
+    nvy = if abs posy >= vborder then -vy else vy
+    ncolor = (currentColor ball+fromEnum (nvx /= vx || nvy /= vy)) `mod` length (colors initialGameState)
+    in Ball {
+        position = position ball,
         velocity = (nvx, nvy),
         currentColor = ncolor
-    }) (balls as),
-    colors = colors as,
-    currentMouseEvent = currentMouseEvent as
-}
+    }
     where
         hborder = fromIntegral scaledWindowWidth / 2 - fromIntegral scaledCircleRadius
         vborder = fromIntegral scaledWindowHeight / 2 - fromIntegral scaledCircleRadius
+
+iterator :: Float -> GameState -> GameState
+iterator deltaTime as = GameState {
+    balls = map (\ball -> let
+       posx = fst $ position ball
+       posy = snd $ position ball
+
+       wallBall = handleWallCollision ball
+       (nvx, nvy) = velocity wallBall
+       nposx = posx + nvx * deltaTime
+       nposy = posy + nvy * deltaTime
+
+       in Ball {
+       position = (nposx, nposy),
+       velocity = velocity wallBall,
+       currentColor = currentColor wallBall
+   }) (balls as),
+    colors = colors as,
+    currentMouseEvent = currentMouseEvent as
+}
 
 fps :: Int
 fps = 60
